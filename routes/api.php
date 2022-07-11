aaa5266d672c44a53f2862db4bee66d536f40e84
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-07-11 17:04:49 +0100

    feat: modulr configuration for laravel

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use App\Http\Controllers\CustomersController;
+
+Route::get('/customers', [CustomersController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
